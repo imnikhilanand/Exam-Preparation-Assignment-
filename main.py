8da7adf361da19026b2774be3f0f58672738afb5
Nikhil Anand <imnikhilanand@gmail.com>
2018-08-04 11:30:22 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -31,10 +31,29 @@ words = set(["inheritance","encapsulation","multithreading"])
 #final string that has to be returned 
 final = ""
 
+#variables to count the occurance of given keywords
+inh=0
+en=0
+mu=0
+
 #returning the final string
 for x in new_str:
+    #counting inheritance occurance    
+    if x == "inheritance":
+        inh=inh+1
+    #counting encapsulation occurance
+    if x == "encapsulation":
+        en=en+1
+    #counting multithreading occurance
+    if x == "multithreading":
+        mu=mu+1
+    #removeing the keywords and appending to final string
     if x not in words:
         final = final +" "+ x;
+    
 
 #printing the string
 print(final)
+
+
+
